086cb47dcb9e052728d24f195dc4fb07d8b40239
Kate <kate2630303@gmail.com>
2024-02-07 20:32:10 +0300

    Исправление задание2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,19 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = b ** 2 - 4 * a * c;
-	if (d === 0) {
-		let x = -b / (2 * a);
-		arr.push(x);
-	} else {
-		let x1 = (-b + Math.sqrt(d)) / (2 * a);
-		let x2 = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(x1, x2);
-	}
-	return arr;
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } 
+  return arr;
 }
 let a = 1;
 let b = 5;
@@ -19,7 +21,7 @@ let c = 4;
 console.log(solveEquation(a, b, c));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  newpercent = percent / 100;
+  let newpercent = percent / 100;
   let monthprocent = newpercent / 12;
   let credit = amount - contribution;
   let monthpayment = credit * (monthprocent + monthprocent / (((1 + monthprocent) ** countMonths) - 1));
